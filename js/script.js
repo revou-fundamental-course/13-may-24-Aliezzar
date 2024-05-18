bfdb7c14e855f3ebc7a19aaa941b3391abb0b94e
Aliezzar <105056102+Aliezzar@users.noreply.github.com>
2024-05-18 14:37:48 -0700

    Mini project with Revou

--- js/script.js
+++ js/script.js
@@ -37,12 +37,12 @@ document.addEventListener("DOMContentLoaded", function() {
 const sisiPertama = document.getElementById('sisi1')
 const sisiKedua = document.getElementById('sisi2')
 const sisiKetiga = document.getElementById('sisi3')
-const hitung = document.getElementById('hitung')
-const output = document.getElementById('output')
-hitung.addEventListener('click', function () {
+const jawab = document.getElementById('hitung')
+const keluar = document.getElementById('output')
+jawab.addEventListener('click', function () {
     let s1 = parseFloat(sisiPertama.value)
     let s2 = parseFloat(sisiKedua.value) 
     let s3 = parseFloat(sisiKetiga.value) 
     let k = s1 + s2 + s3
-    output.innerHTML = `Diketahui sisi pertama ${s1} cm, sisi kedua ${s2} cm, sisi ketiga ${s3} cm, maka kelilingnya adalah ${k} cm`
+    keluar.innerHTML = `Diketahui sisi pertama ${s1} cm, sisi kedua ${s2} cm, sisi ketiga ${s3} cm, maka kelilingnya adalah ${k} cm`
 })
